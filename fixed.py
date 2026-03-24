0b51b1a19c85c58f30c6c468e6230648fca197a2
Wenyijie Zhao <zhaowenyijie@gmail.com>
2026-03-24 04:36:47 +0000

    conditioned final version

---
+++ fixed.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+# INET4031
+# Wenyijie Zhao
+# March 23 2026 Created
+# Modified at the same day.
+
+#Importing os for os.system, where that will let python run commands for us.
+#Importing re , regular expression, we can use the match function to identify the pounds so the interpreter knows 
+# that these are not the lines to scan.
+# import that sys is just for that stdin can work correctly.
+import os
+import re
+import sys
+
+# a main function, where the code will do, as a function .
+
+
+
+def main():
+# attempting to use tty to get around the standard input
+    tty = open ("/dev/tty")
+    print("Dry Run (Y/N)?")
+    mode = tty.readline().strip().upper()
+
+    for line in sys.stdin:
+
+        #Regular expression here, with ^ which means starting with, in this case, anything starts with hashtag.
+        match = re.match("^#",line)
+
+        #The code splits each line into small pieces, when seeing a colon they separate it. 
+        # strip is for getting rid of unnecessary elements, like escape sequences or so
+        fields = line.strip().split(':')
+
+        # Or gate and a not gate, or something like that
+        # the statement is checking  whether it is a comment (line starting with hashtag above) or numbers of fields
+        # the number has to equal to five, else that will be skipped.
+        # if the expression turns out to be true, then it will be skipped by continue command.
+        # This if statement rely on two variable: match and fields.
+        # Has to equal to five, since that is how much categories of info needed from a user to build a profile.
+        if match:
+            if mode == "Y":
+                print("Skipping Line",line)
+            continue
+
+        if len(fields) != 5:
+            if mode == "Y":
+                print("Invalid Line:",line)
+            continue
+
+        #Purpose of the next 3 lines: to assign a variable name ( so they can be recalled easily later)
+        # and set them into the desired info : username, password, etc. %s %s are a place holder where field[3] and 
+        # field [2] will be fitting in, depending on the given input.
+        # password info will be put into the fields[1], that's the expected place to find the password
+        # gecos, a linux personal info identifier. Last name and first name.
+        username = fields[0]
+        password = fields[1]
+        gecos = "%s %s,,," % (fields[3],fields[2])
+
+        # There can be more than one group. splitting that in case if there are more than one group separated by ","
+        groups = fields[4].split(',')
+
+        #Letting user see the interaction, and letting the developers to debug. For visual purposes.
+        print("==> Creating account for %s..." % (username))
+        # this thing adds user, and make password uninteractive in the beginning, and add gecos and username in the end.
+        cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
+
+        # No significant error on dry run
+        # os.system will execute the command above to create users.
+
+    if mode == "Y":
+        print(cmd)
+    elif mode == "N":
+            #run = input("wanna run? (Y/N)") abandoned....
+        os.system(cmd)
+    else:
+        print("what are you typing??? try again with Y or N.")
+
+        # Letting whoever interacts to know that this function has functioned til this far. for visual purpose and debugging
+
+        print("==> Setting the password for %s..." % (username))
+        
+    # /bin/echo outpus the strings,n for not auto changing lines and -e to enable \ n, and that double percent s makes it 
+        # twice of the input of the parameters, which is password, as you can see the two password param at end
+        # pipe operator is just  whatever that is on the left will be sent to usr/bin/sudo,and usr/bin/passwd
+        # or that it takes output of the echo send it as input of the passwd command
+        cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
+
+        #the system will run cmd to set passwords when uncommented.
+        if mode == "Y":
+            print(cmd)
+        elif mode == "N":
+            os.system(cmd)
+        else:
+             print("Invalid input. choose Y/N")
+
+        for group in groups:
+            # this for statement is trying to assign groups. the if statement is making a base case, since '-'
+            # means that there isn't an assignable group for this user.
+            if group != '-':
+                print("==> Assigning %s to the %s group..." % (username,group))
+                cmd = "/usr/sbin/adduser %s %s" % (username,group)
+                if mode == "Y":
+            print(cmd)
+                elif mode == "N":
+            os.system(cmd)
+        else:
+            print("Invalid. choose Y/N")
+
+
+    tty.close()
+# classic stuff
+if __name__ == '__main__':
+    main()
